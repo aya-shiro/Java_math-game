8e2fd1fe8e00c27e6d58fbd5872008ab6e9abd0c
aya-shiro <whitecheck00@gmail.com>
2023-06-09 09:45:41 +0000

    [update]Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    if level  0 {
+    Me me = new Me("YOU", 0, 1);
+    if (me.level == 0) {
+      System.out.print("Start!");
       System.out.println("Level 1");
     } else {
       System.out.println("Level" + this.level);
     }
     
-    TodoList todo = null;
+    // TodoList todo = null;
 
     System.out.print("どの敵を倒す？：");
-    String title = scanner.next();
-    System.out.print(title + "の期限：");
+    String enemyName = scanner.next();
+    System.out.print(Enmy.arms + "の武器を落とした！");
     int limit = scanner.nextInt();
+  }
+}
