01dafe2b576bf6913230c6b4f38b8a2debd911e7
aya-shiro <whitecheck00@gmail.com>
2023-06-16 12:20:45 +0000

    [update]Enemy

--- Enemy.java
+++ Enemy.java
@@ -7,33 +7,36 @@ public class Enemy {
   public static void firstMatch(int level, int totalArms){
     for(level=1; level <= 10; level++){
       Scanner scanner = new Scanner(System.in);
-      int randomEnemyStatus1 = (int)(Math.random()*100);
-      int randomEnemyStatus2 = (int)(Math.random()*100);
+      int randomEnemyStatus1 = (int)(Math.random()+100);
+      int randomEnemyStatus2 = (int)(Math.random()+100);
       int randomEnemyStatus3 = (int)(Math.random() * (totalArms + 1));  //totalArms以下でランダム数値生成
 
       System.out.print("敵A:" + totalArms + randomEnemyStatus1 + " " );
       System.out.print("敵B:" + totalArms + randomEnemyStatus2 + " " );
       System.out.println("敵C:" + randomEnemyStatus3 );
-      
+
       System.out.print("どの敵を倒す？：");
       String enemyName = scanner.next();
-      if (enemyName == "A") {
+      // if (enemyName == "A") {  ==演算子は参照先の比較時に使う
+      if (enemyName.equals("A")) {    //文字列の比較にはequals("OO")を使う！
         totalArms += randomEnemyStatus1;
-      } else if (enemyName == "B") {
+      } else if (enemyName.equals("B")) {
         totalArms += randomEnemyStatus2;
-      } else if (enemyName == "C") {
+      } else if (enemyName.equals("C")) {
         totalArms += randomEnemyStatus3;
       } else {
-        // ABC以外を選んだ場合にやり直させる処理
+        System.out.println("敵A・敵B・Cの中から選び直してください");
+        level -=1;  //forで加算された1を打ち消してcontinue;でやり直し
+        continue;
       }
-      
+
       System.out.println(enemyName + "を倒した！");
       System.out.println("UP to Level " + level);
       System.out.println(enemyName + "の武器値" +  "を自分の武器に加算した！");
-      
+
     }
   }
-  
+
   // public static int enemyMatch(){
   //   super.firstMatch();
   //   System.out.println("Level" + level);
