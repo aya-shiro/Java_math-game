f0fbe38c389c9c6ea867bf5bb13decfd2c1eeeba
aya-shiro <whitecheck00@gmail.com>
2023-06-21 11:28:52 +0000

    [update]Enemy

--- Enemy.java
+++ Enemy.java
@@ -2,29 +2,27 @@ import java.util.Scanner;
 
 public class Enemy {
   // private String enemyName;
-  private int arms;
+  // private int arms;
 
   public static void Match(int level, int totalArms){
     for(level=1; level <= 10; level++){
       Scanner scanner = new Scanner(System.in);
-      int randomEnemyStatus1 = (int)(Math.random()+100);
-      int randomEnemyStatus2 = (int)(Math.random()+100);
-      int randomEnemyStatus3 = (int)(Math.random() * (totalArms + 1));  //totalArms以下でランダム数値生成
+      int min = 1;
+      int max = 20;
+      int randomEnemyStatus = (int) Math.floor(Math.random() + (max - min + 1) + min);
 
-      System.out.print("敵A:" + totalArms + randomEnemyStatus1 + " " );
-      System.out.print("敵B:" + totalArms + randomEnemyStatus2 + " " );
-      System.out.println("敵C:" + randomEnemyStatus3 );
+
+      System.out.print("敵A:" + (totalArms + randomEnemyStatus) + " " );
+      System.out.print("敵B:" + (totalArms + randomEnemyStatus) + " " );
+      System.out.println("敵C:" + (totalArms - randomEnemyStatus));
 
       System.out.print("どの敵を倒す？：");
       String enemyName = scanner.next();
 
       // if (enemyName == "A") {  ==演算子は参照先の比較時に使う
-      if (enemyName.equals("A")) {    //文字列の比較にはequals("OO")を使う！
-        totalArms += randomEnemyStatus1;
-      } else if (enemyName.equals("B")) {
-        totalArms += randomEnemyStatus2;
-      } else if (enemyName.equals("C")) {
-        totalArms += randomEnemyStatus3;
+      if (enemyName.equals("A") || enemyName.equals("B") || enemyName.equals("C")) {
+       //文字列の比較にはequals("OO")を使う！
+        // totalArms += randomEnemyStatus;
       } else {
         System.out.println("敵A・敵B・Cの中から選び直してください");
         System.out.println("-------------------------");
@@ -32,10 +30,12 @@ public class Enemy {
         continue;
       }
 
-      if (totalArms > randomEnemyStatus1) {
+      if (totalArms > randomEnemyStatus) {
         System.out.println(enemyName + "を倒した！");
         System.out.println("Level" + level + "になった");
         System.out.println(enemyName + "の武器値" +  "を自分の武器に加算した！");
+        totalArms += randomEnemyStatus;
+
         System.out.println("-------------------------");
       } else {
         System.out.println("負け");
