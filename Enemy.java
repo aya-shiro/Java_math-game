ceb357871085efe480b5194a75a44f294e1adcbf
aya-shiro <whitecheck00@gmail.com>
2023-06-21 12:51:32 +0000

    [inTheMiddle]Enemy

--- Enemy.java
+++ Enemy.java
@@ -30,33 +30,30 @@ public class Enemy {
       String enemyName = scanner.next();
 
       // ==演算子は参照先の比較時に使う 文字列の比較にはequals("OO")を使う！
-
       if (enemyName.equals("A") || enemyName.equals("B") || enemyName.equals("C")) {
         // totalArms += randomEnemyStatus;
-        if (totalArms > randomEnemyStatus) {
-          System.out.println(enemyName + "を倒した！");
-          System.out.println("Level" + level + "になった");
-          System.out.println(enemyName + "の武器値" +  "を自分の武器に加算した！");
-          totalArms = myArm + randomEnemyStatus;
-          System.out.println("今の自分の武器数値：" + totalArms);
-          System.out.println("-------------------------");
-        } else {
-          System.out.println("負け");
-          break;
-        }
+        selectedEnemy(totalArms, randomEnemyStatus, enemyName, level, myArm);
       } else {
         System.out.println("敵A・敵B・Cの中から選び直してください");
         System.out.println("-------------------------");
         level -=1;  //forで加算された1を打ち消してcontinue;でやり直し
         continue;
       }
-
-
-
     }
   }
-
-  // public static int myTotalArms1() {
-  //   return totalArms + randomEnemyStatus1;  //自分の武器値に倒した敵の武器値を加算
-  // }
+  
+  public static void selectedEnemy(int totalArms, int randomEnemyStatus, String enemyName, int level, int mrArm) {  //敵A敵B敵Cのどれかを選んだ先の勝ち負け判定
+    if (totalArms > randomEnemyStatus) {
+      System.out.println(enemyName + "を倒した！");
+      System.out.println("Level" + level + "になった");
+      System.out.println(enemyName + "の武器値" +  "を自分の武器に加算した！");
+      totalArms = myArm + randomEnemyStatus;
+      System.out.println("今の自分の武器数値：" + totalArms);
+      System.out.println("-------------------------");
+    } else {
+      System.out.println("負け");
+      break;
+    }
+  }
+  
 }
