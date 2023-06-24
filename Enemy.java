eb043490746a2b67c1e66371fd59a41aea10c047
aya-shiro <whitecheck00@gmail.com>
2023-06-24 11:59:44 +0000

    [update]Enemy

--- Enemy.java
+++ Enemy.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Enemy {
   // private String enemyName;
   private static int myArm;
-  
+
   public static void setMyArm(int value) {
     myArm = value;
   }
-  
+
   public static int getMyArm() {
     return myArm;
   }
@@ -15,9 +15,8 @@ public class Enemy {
   public static void Match(int level, int totalArms){
     for(level=1; level <= 10; level++){
       Scanner scanner = new Scanner(System.in);
-      int min = 1;
-      int max = 20;
-      int randomEnemyStatus = (int) Math.floor(Math.random() * (max - min + 1) + min);
+
+      int randomEnemyStatus = (int) (Math.random() * 10);
 
       int[] enemyArms = {totalArms + randomEnemyStatus, totalArms + randomEnemyStatus, totalArms - randomEnemyStatus};
       System.out.print("敵A:" + enemyArms[0] + " " );
@@ -38,23 +37,27 @@ public class Enemy {
       }
     }
   }
-  
+
   //敵A敵B敵Cのどれかを選んだ先の勝ち負け判定
   public static void selectedEnemy(int totalArms, int randomEnemyStatus, String enemyName, int level, int myArm, int[] enemyArms) {
+    System.out.println("今の自分の武器数値：" + totalArms);
+    boolean defeated = false; // 勝利フラグの初期化
+
     for (int i = 0; i < enemyArms.length; i++) {
       if (totalArms > enemyArms[i]) {
         System.out.println(enemyName + "を倒した！");
         System.out.println("Level" + level + "になった");
-        System.out.println(enemyName + "の武器値" +  "を自分の武器に加算した！");
-        // totalArms = myArm + randomEnemyStatus;
+        System.out.println(enemyName + "の武器値を自分の武器に加算した！");
         totalArms += enemyArms[i];
         System.out.println("今の自分の武器数値：" + totalArms);
         System.out.println("-------------------------");
-      } else {
-        System.out.println("負け");
-        break;
+        defeated = true; // 勝利した場合、勝利フラグをtrueに設定
       }
     }
+
+    if (!defeated) {
+      System.out.println("負け");
+    }
   }
-  
+
 }
